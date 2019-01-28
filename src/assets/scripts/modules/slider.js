84d45e9c3da59cca5fddb5896319b4aae3b0db73
Alexander Shuvalov <ashuv_vyksa@mail.ru>
2019-01-28 15:32:39 +0300

    simple slider

--- src/assets/scripts/modules/slider.js
+++ src/assets/scripts/modules/slider.js
@@ -1,38 +1,31 @@
 export default function Slider(option){
-    console.log("dfgndfgk");
+
     const left = document.querySelector(option.l);
     const right = document.querySelector(option.r);
     const slider = document.querySelector(option.list);
-    const computed = getComputedStyle(slider);
-    const sliderWidth = parseInt(getComputedStyle(slider).width);
+
     var sliderItemsAmount = slider.children.length;
+    var sliderItemWidth = parseInt(getComputedStyle(slider).width);
+    var currentSlide = 0;
     
     right.addEventListener("click", function(e) {
       e.preventDefault();
-      let currentRight = parseInt(computed.right);
-    
-      if (!currentRight) {
-        currentRight = 0;
+      currentSlide++;
+
+
+      if (currentSlide > sliderItemsAmount-1) {
+        currentSlide = 0;
       }
-      if ((currentRight%sliderWidth==0)||(currentRight==0)){
-        if (currentRight < (sliderItemsAmount-1)*sliderWidth) {
-          slider.style.right = currentRight + sliderWidth + "px";
-        }
-      }
+      slider.style.right = currentSlide*sliderItemWidth + "px";
     });
     
     left.addEventListener("click", function(e) {
       e.preventDefault();
-      let currentRight = parseInt(computed.right);
-    
-      if (!currentRight) {
-        currentRight = 0;
+      currentSlide--;
+      if (currentSlide < 0) {
+        currentSlide = sliderItemsAmount-1;
       }
-      if ((currentRight%sliderWidth==0)||(currentRight==0)){
-        if (currentRight > 0) {
-          slider.style.right = currentRight - sliderWidth + "px";
-        }
-      }
+      slider.style.right = currentSlide*sliderItemWidth + "px";
     });
     
 }
