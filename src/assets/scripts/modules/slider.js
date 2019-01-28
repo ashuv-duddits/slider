9a35eb255f0dc8d1d307c131bb0ac15a4b3c31b6
Alexander Shuvalov <ashuv_vyksa@mail.ru>
2019-01-28 21:06:03 +0300

    fix threeSlidesAnimation

--- src/assets/scripts/modules/slider.js
+++ src/assets/scripts/modules/slider.js
@@ -7,99 +7,108 @@ export default function Slider(option) {
 
   var sliderItems = Array.from(slider.children);
 
+  let isAnimate = false;
+
   //Кнопки управления слайдером
 
   right.addEventListener("click", function (e) {
     e.preventDefault();
+    if (!isAnimate) {
+      let animation = function () {
+        return new Promise(function (resolve) {
+          //Функция анимации
+          let animate = function(options) {
 
+            var start = performance.now();
 
-    let animation = function () {
-      return new Promise(function (resolve) {
-        //Функция анимации
-        function animate(options) {
+            requestAnimationFrame(function animate(time) {
+              // timeFraction от 0 до 1
+              var timeFraction = (time - start) / options.duration;
+              if (timeFraction > 1) timeFraction = 1;
 
-          var start = performance.now();
+              // текущее состояние анимации
+              var progress = options.timing(timeFraction)
 
-          requestAnimationFrame(function animate(time) {
-            // timeFraction от 0 до 1
-            var timeFraction = (time - start) / options.duration;
-            if (timeFraction > 1) timeFraction = 1;
+              options.draw(progress);
 
-            // текущее состояние анимации
-            var progress = options.timing(timeFraction)
-
-            options.draw(progress);
-
-            if (timeFraction < 1) {
-              requestAnimationFrame(animate);
-            } else {
-              resolve();
+              if (timeFraction < 1) {
+                isAnimate = true;
+                requestAnimationFrame(animate);
+              } else {
+                isAnimate = false;
+                resolve();
+              }
+            });
+          }
+          animate({
+            duration: 500,
+            timing: function (timeFraction) {
+              return Math.pow(timeFraction, 2);
+            },
+            draw: function (progress) {
+              slider.style.right = progress * widthItemSlider + 'px';
             }
           });
-        }
-        animate({
-          duration: 500,
-          timing: function (timeFraction) {
-            return Math.pow(timeFraction, 2);
-          },
-          draw: function (progress) {
-            slider.style.right = progress * widthItemSlider + 'px';
-          }
-        });
+        })
+      }
+
+      animation().then(function () {
+        let deleteElem = sliderItems.shift();
+        sliderItems.push(deleteElem);
+        slider.innerHTML = "";
+        sliderItems.forEach((item) => {
+          slider.appendChild(item);
+        })
+        slider.style.right = 0 + 'px';
       })
     }
-
-    animation().then(function () {
-      let deleteElem = sliderItems.shift();
-      sliderItems.push(deleteElem);
-      slider.innerHTML = "";
-      sliderItems.forEach((item) => {
-        slider.appendChild(item);
-      })
-      slider.style.right = 0 + 'px';
-    })
 
   });
 
   left.addEventListener("click", function (e) {
     e.preventDefault();
-
-    let deleteElem = sliderItems.pop();
-    sliderItems.unshift(deleteElem);
-    slider.innerHTML = "";
-    sliderItems.forEach((item) => {
-      slider.appendChild(item);
-    })
-    slider.style.right = widthItemSlider + 'px';
-
-    //Функция анимации
-    function animate(options) {
-
-      var start = performance.now();
-
-      requestAnimationFrame(function animate(time) {
-        // timeFraction от 0 до 1
-        var timeFraction = (time - start) / options.duration;
-        if (timeFraction > 1) timeFraction = 1;
-
-        // текущее состояние анимации
-        var progress = options.timing(timeFraction)
-
-        options.draw(progress);
-
-        if (timeFraction < 1) {
-          requestAnimationFrame(animate);
+    if (!isAnimate) {
+      let deleteElem = sliderItems.pop();
+      sliderItems.unshift(deleteElem);
+      slider.innerHTML = "";
+      sliderItems.forEach((item) => {
+        slider.appendChild(item);
+      })
+      slider.style.right = widthItemSlider + 'px';
+  
+      //Функция анимации
+      let animate = function(options) {
+  
+        var start = performance.now();
+  
+        requestAnimationFrame(function animate(time) {
+          // timeFraction от 0 до 1
+          var timeFraction = (time - start) / options.duration;
+          if (timeFraction > 1) timeFraction = 1;
+  
+          // текущее состояние анимации
+          var progress = options.timing(timeFraction)
+  
+          options.draw(progress);
+  
+          if (timeFraction < 1) {
+            isAnimate = true;
+            requestAnimationFrame(animate);
+          }else{
+            isAnimate = false;
+          }
+        });
+      }
+      animate({
+        duration: 500,
+        timing: function (timeFraction) {
+          return Math.pow(timeFraction, 2);
+        },
+        draw: function (progress) {
+          slider.style.right = (1 - progress) * widthItemSlider + 'px';
         }
       });
     }
-    animate({
-      duration: 500,
-      timing: function (timeFraction) {
-        return Math.pow(timeFraction, 2);
-      },
-      draw: function (progress) {
-        slider.style.right = (1 - progress) * widthItemSlider + 'px';
-      }
-    });
+
   });
 }
