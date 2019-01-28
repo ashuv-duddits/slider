dbae1cb9fc43c14f8dc919d3483b873c567ef14a
Alexander Shuvalov <ashuv_vyksa@mail.ru>
2019-01-28 19:46:06 +0300

    threeSlides complete

--- src/assets/scripts/modules/slider.js
+++ src/assets/scripts/modules/slider.js
@@ -4,28 +4,27 @@ export default function Slider(option){
     const right = document.querySelector(option.r);
     const slider = document.querySelector(option.list);
 
-    var sliderItemsAmount = slider.children.length;
-    var sliderItemWidth = parseInt(getComputedStyle(slider).width);
-    var currentSlide = 0;
+    var sliderItems = Array.from(slider.children);
     
     right.addEventListener("click", function(e) {
       e.preventDefault();
-      currentSlide++;
-
-
-      if (currentSlide > sliderItemsAmount-1) {
-        currentSlide = 0;
-      }
-      slider.style.right = currentSlide*sliderItemWidth + "px";
+      let deleteElem = sliderItems.shift();
+      sliderItems.push(deleteElem);
+      slider.innerHTML = "";
+      sliderItems.forEach((item)=>{
+        slider.appendChild(item);
+      })
+      
     });
     
     left.addEventListener("click", function(e) {
       e.preventDefault();
-      currentSlide--;
-      if (currentSlide < 0) {
-        currentSlide = sliderItemsAmount-1;
-      }
-      slider.style.right = currentSlide*sliderItemWidth + "px";
+      let deleteElem = sliderItems.pop();
+      sliderItems.unshift(deleteElem);
+      slider.innerHTML = "";
+      sliderItems.forEach((item)=>{
+        slider.appendChild(item);
+      })
     });
     
 }
